f6c98711795e7f6c4bbf61a754c1857ea1508ad7
Julián Alejandro Mosquera <jumosquera@frba.utn.edu.ar>
2024-10-04 16:07:13 -0300

    .

--- src/main/java/ar/edu/utn/dds/k3003/clients/ViandasRetrofitClient.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/ViandasRetrofitClient.java
@@ -1,14 +1,16 @@
 package ar.edu.utn.dds.k3003.clients;
 
+import ar.edu.utn.dds.k3003.facades.dtos.EstadoViandaEnum;
 import ar.edu.utn.dds.k3003.facades.dtos.ViandaDTO;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
+import retrofit2.http.*;
 
 public interface ViandasRetrofitClient {
 
     @GET("viandas/{qr}")
     Call<ViandaDTO> get(@Path("qr") String qr);
+
+    @PATCH("viandas/{qr}/estado")
+    Call<ViandaDTO> modificarEstado(@Path("qr") String qr, @Body EstadoViandaEnum status);
 
 }
