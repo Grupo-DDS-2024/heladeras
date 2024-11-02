62950794af7dac813e11d6e7d4b2845c1eeaf4ef
FedericoBruni <fbruni@fi.uba.ar>
2024-11-01 22:24:11 -0300

    Get heladeras muestra el estado

--- src/main/java/ar/edu/utn/dds/k3003/controller/ObtenerHeladeraController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/ObtenerHeladeraController.java
@@ -2,12 +2,15 @@ package ar.edu.utn.dds.k3003.controller;
 
 import ar.edu.utn.dds.k3003.app.Fachada;
 import ar.edu.utn.dds.k3003.facades.dtos.HeladeraDTO;
+import ar.edu.utn.dds.k3003.model.Heladera;
 import ar.edu.utn.dds.k3003.repositories.HeladeraJPARepository;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class ObtenerHeladeraController {
@@ -32,10 +35,16 @@ public class ObtenerHeladeraController {
 
 
             HeladeraDTO heladeraDTO = this.fachadaHeladera.buscarXId((Integer.parseInt(context.pathParam("id"))));
+            Heladera heladera = this.heladeraRepository.findById(Integer.parseInt(context.pathParam("id")));
             em.getTransaction().commit();
             em.close();
-            context.status(200).json(heladeraDTO);
+
             //context.status(200).json(heladeraDTO); // Falta agregar la cantidad de viandas depositadas actualmente
+            Map<String, Object> response = new HashMap<>();
+            response.put("HeladeraDTO", heladeraDTO);
+            response.put("Heladera estado", heladera.getEstadoHeladera() );
+            response.put("Heladera viandas actuales", heladera.getCantidadViandas());
+            context.status(200).json(response);
 
         } catch (NoSuchElementException e) {
             em.getTransaction().commit();
